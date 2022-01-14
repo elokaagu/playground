5178b7951b02c0cbb59adcf73895a18159b5fb15
Eloka <12972250+elokaagu@users.noreply.github.com>
2022-01-14 14:22:55 +0000

    framer motion

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+      colors: {
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        main: "var(--main)",
+        background: "var(--background)",
+        header: "var(--header)",
+        accent: "var(--accent)",
+      },
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      extend: {
+        spacing: {
+          128: "32rem",
+          144: "36rem",
+        },
+        borderRadius: {
+          "4xl": "2rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
